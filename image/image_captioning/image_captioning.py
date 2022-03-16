aa7d3661213ba6e45cbe7b64d654c962991cd4bf
umnooob <dzx60090@gmail.com>
2022-03-16 20:48:54 +0800

    init

---
+++ image/image_captioning/image_captioning.py
@@ -0,0 +1,78 @@
+import datetime
+import torch
+import torchvision.transforms as transforms
+
+from django.conf import settings
+from PIL import Image
+import pickle
+import math
+import io
+import os
+from .models import EncoderCNN, DecoderRNN
+
+
+#hyperparameters
+EMBEDDING_DIM = 256
+HIDDEN_DIM = 512
+NUM_LAYERS = 2
+BEAM_SIZE = 3
+MAX_SEG_LENGTH = 20
+ID_TO_WORD_PATH = os.path.join(settings.STATIC_ROOT, 'vocab/id_to_word.pkl')
+with open(ID_TO_WORD_PATH, 'rb') as f:
+        ID_TO_WORD = pickle.load(f)
+END_ID = [k for k, v in ID_TO_WORD.items() if v == '<end>'][0]
+VOCAB_SIZE = len(ID_TO_WORD)
+ENCODER_PATH = os.path.join(settings.STATIC_ROOT, 'model/encoder.pth')
+DECODER_PATH = os.path.join(settings.STATIC_ROOT, 'model/decoder.pth')
+
+# load models only once
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("Running in %s." % device)
+encoder = EncoderCNN(EMBEDDING_DIM)
+encoder = encoder.to(device).eval()
+
+decoder = DecoderRNN(EMBEDDING_DIM, HIDDEN_DIM, VOCAB_SIZE, NUM_LAYERS, MAX_SEG_LENGTH)
+decoder = decoder.to(device).eval()
+
+# Load the trained model parameters
+encoder.load_state_dict(torch.load(ENCODER_PATH))
+decoder.load_state_dict(torch.load(DECODER_PATH))
+
+def transform_image(image_bytes):
+    """
+    Transform image into required DenseNet format: 224x224 with 3 RGB channels and normalized.
+    Return the corresponding tensor.
+    """
+    my_transforms = transforms.Compose([transforms.Resize([224, 224], Image.LANCZOS),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+    return my_transforms(image).unsqueeze(0)
+
+def inference(image_bytes):
+    """For given image bytes, output the cations and probilities using the EncoderCNN and DecoderRNN"""
+    # Choose Device
+    
+    # Prepare an image
+    image = transform_image(image_bytes).to(device)
+
+    # Generate an caption from the image
+    with torch.no_grad():
+        feature = encoder(image)
+        sampled_ids = decoder.beam_search(feature, BEAM_SIZE, END_ID)
+    res=[]
+    # Convert word_ids to words
+    for i, (sampled_id, prob) in enumerate(sampled_ids):
+        sampled_id = sampled_id.cpu().numpy()
+        sampled_caption = []
+        for word_id in sampled_id:
+            word = ID_TO_WORD[word_id]
+            if word != '<end>' and word != '<start>':
+                sampled_caption.append(word)
+            if word == '<end>':
+                break
+        sentence = ' '.join(sampled_caption)
+        res.append((sentence, math.exp(prob.item())*100))
+    return res
---
+++ image/views.py
@@ -0,0 +1,83 @@
+import base64
+import io
+import json
+import os
+import torch
+from torchvision import models
+from torchvision import transforms
+from PIL import Image
+from django.shortcuts import render
+from django.conf import settings
+
+from .forms import ImageUploadForm
+from .image_captioning.image_captioning import inference
+
+# PyTorch-related code from: https://pytorch.org/tutorials/intermediate/flask_rest_api_tutorial.html
+# load pretrained DenseNet and go straight to evaluation mode for inference
+# load as global variable here, to avoid expensive reloads with each request
+
+
+
+
+# def transform_image(image_bytes):
+#     """
+#     Transform image into required DenseNet format: 224x224 with 3 RGB channels and normalized.
+#     Return the corresponding tensor.
+#     """
+#     my_transforms = transforms.Compose([transforms.Resize(255),
+#                                         transforms.CenterCrop(224),
+#                                         transforms.ToTensor(),
+#                                         transforms.Normalize(
+#                                             [0.485, 0.456, 0.406],
+#                                             [0.229, 0.224, 0.225])])
+#     image = Image.open(io.BytesIO(image_bytes))
+#     return my_transforms(image).unsqueeze(0)
+
+
+# def get_prediction(image_bytes):
+#     """For given image bytes, predict the label using the pretrained DenseNet"""
+#     tensor = transform_image(image_bytes)
+#     outputs = model.forward(tensor)
+    
+    
+#     percentage = torch.nn.functional.softmax(outputs, dim=1)[0] * 100
+#     _, indices = torch.sort(outputs, descending=True)
+#     # _, y_hat = outputs.max(1)
+#     res=[(imagenet_mapping[str(idx.item())][1], percentage[idx.item()].item()) for idx in indices[0][:5]]
+#     # predicted_idx = str(y_hat.item())
+#     # class_name, human_label = imagenet_mapping[predicted_idx]
+#     return res
+
+
+def index(request):
+    image_uri = None
+    predicted_res = None
+
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            # passing the image as base64 string to avoid storing it to DB or filesystem
+            image = form.cleaned_data['image']
+            image_bytes = image.file.read()
+            encoded_img = base64.b64encode(image_bytes).decode('ascii')
+            image_uri = 'data:%s;base64,%s' % ('image/jpeg', encoded_img)
+
+            # get predicted label
+            try:
+                predicted_res = inference(image_bytes)
+            except RuntimeError as re:
+                print(re)
+                # predicted_label = "Prediction Error"
+
+    else:
+        form = ImageUploadForm()
+
+    context = {
+        'form': form,
+        'image_uri': image_uri,
+        'predicted_res': predicted_res,
+    }
+    return render(request, 'image/index.html', context)
+
+if __name__ == '__main__':
+    print(inference(open("D:\R.jpg", 'rb').read()))
