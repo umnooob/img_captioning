a2b86e95fbfbe06b371683257951410b5dd409c9
umnooob <dzx60090@gmail.com>
2022-03-17 13:16:54 +0800

    debugging

--- pytorch_django/prod_settings.py
+++ pytorch_django/prod_settings.py
@@ -4,14 +4,14 @@ import django_heroku
 # default: use settings from main settings.py if not overwritten
 from .settings import *
 
-
+from django.core.management.utils import get_random_secret_key
 ############
 # SECURITY #
 ############
 
 DEBUG = False
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 ALLOWED_HOSTS = ['img-captioning.herokuapp.com']
 
